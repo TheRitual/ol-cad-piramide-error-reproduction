1c07fc5b71cb47a5d26745a1c369e7adf2968370
Igor Buczak <igor.buczak@skysnap.pl>
2024-02-09 15:35:48 +0100

    Fix Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ol-cad-piramide-error-reproduction/",
+  // base: "/ol-cad-piramide-error-reproduction/",
   plugins: [react()],
 })
